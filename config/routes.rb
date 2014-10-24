9a0c4b1b38a18fd89635b2ac7380c6e47b45f8d4
Ignacio Ureta <trucoso@gmail.com>
2014-10-24 19:40:02 +0000

    welcome page created, bootstrap added and some stylings into ideas and operations pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+
+  #root 'welcome#index'
+  
   resources :operations
 
   resources :ideas
@@ -11,7 +15,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
